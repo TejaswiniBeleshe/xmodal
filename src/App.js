a87ee083103be9742a459bb2ec285bfdd65f18f9
TejaswiniBeleshe <sonu2201.beleshe@gmail.com>
2024-09-28 22:59:25 +0530

    change made in app.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import './App.css';
+import styles from "./App.module.css"
 import Model from './Model';
 
 function App() {
   const [state,setState] = useState(false)
   return (
-    <div className="modal">
+    <div className={styles.modal}>
       <h1>User Details Model</h1>
-      <button id="openbtn" onClick={()=>setState(true)}>Open Form</button>
+      <button id={styles.openbtn} onClick={()=>setState(true)}>Open Form</button>
       {state?<Model setState={setState}/>:""}
       
     </div>
